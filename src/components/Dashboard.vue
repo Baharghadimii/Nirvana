6005460bfc86821977de1a8bb5d37c0d2e915063
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-04-29 17:09:27 -0700

    make it ready ;)

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ subj }}</h1>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  props: {
+    msg: String,
+    subj: String
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+  font-size: 4rem;
+}
+p {
+  font-size: 2rem;
+  color: blue;
+}
+</style>
