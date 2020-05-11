701c190bdbe67c1ddf2cadaa2f0f9ba6352392ca
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-05-11 16:49:40 -0700

    set up search bar component

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,24 +1,17 @@
 <template>
-  <div class="dashborad"></div>
+  <div class="dashborad">
+    <SearchBar />
+  </div>
 </template>
 
 <script>
+import SearchBar from "./SearchBar.vue";
 export default {
   name: "Dashboard",
-  props: {
-    msg: String,
-    subj: String
+  components: {
+    SearchBar
   }
 };
 </script>
 
-<style scoped>
-h1 {
-  margin: 40px 0 0;
-  font-size: 4rem;
-}
-p {
-  font-size: 2rem;
-  color: blue;
-}
-</style>
+<style scoped></style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="search-bar"></div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  props: {}
+};
+</script>
+
+<style lang="scss">
+.search-bar {
+  background-color: $secondary;
+}
+</style>
