38c2a05c7f3006e21b0ebddead4b38a08fe6f146
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-05-13 14:04:22 -0700

    added input and value

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="search-div">
     <div id="box" class="search-box" v-on:click="expand">
+      <input id="search-input" type="text" hidden value="Search" />
       <font-awesome-icon
         icon="search"
         size="lg"
-        style="color:#21202e;margin:0.6rem;float:right"
+        style="color:#2e2b3f;margin:0.6rem;float:right"
       />
     </div>
   </div>
@@ -17,6 +18,10 @@ export default {
   methods: {
     expand: function() {
       document.getElementById("box").className += " expanded";
+      //handle different timing for transition
+      setTimeout(() => {
+        document.getElementById("search-input").className = "expanded-input";
+      }, 500);
     }
   }
 };
@@ -27,6 +32,7 @@ export default {
   width: $full;
   height: 10%;
   position: relative;
+  background-color: $secondary;
 }
 .search-box {
   top: 20%;
@@ -34,13 +40,30 @@ export default {
   width: 2.5em;
   height: 2.5em;
   border-radius: 50%;
-  background-color: $secondary;
   position: absolute;
   cursor: pointer;
+  background-color: $primary;
   transition: width 1s;
 }
+#search-input {
+  width: 8em;
+  height: 2.5em;
+  background-color: transparent;
+  border: none;
+  color: $secondary;
+  font-size: 1rem;
+  font-weight: 700;
+  float: left;
+  padding: 0 0 1.5rem 1rem;
+  transition: all 5s;
+}
+//after transition
+.expanded-input {
+  visibility: visible;
+  display: inline-block;
+}
 .expanded {
-  width: 10em;
+  width: 15em;
   border-radius: 20px;
 }
 </style>
