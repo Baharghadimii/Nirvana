e020a21967a2c58d31b6bae579f0eb48b8e57a49
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-05-13 18:00:28 -0700

    added hover for icons--background changes

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,33 +1,49 @@
 <template>
   <div class="servicePanel">
     <div class="icon-holder">
-      <font-awesome-icon icon="list-ul" style="color:#21202e;" size="2x" />
+      <font-awesome-icon
+        icon="list-ul"
+        style="color:#21202e;padding:0.5rem 0"
+        size="2x"
+      />
       <span class="tooltip">Watclist</span>
     </div>
     <div class="icon-holder">
-      <font-awesome-icon icon="fire" style="color:#21202e;" size="2x" />
+      <font-awesome-icon
+        icon="fire"
+        style="color:#21202e;padding:0.5rem 0"
+        size="2x"
+      />
       <span class="tooltip">Hotlist</span>
     </div>
     <div class="icon-holder">
-      <font-awesome-icon icon="comments" style="color:#21202e;" size="2x" />
+      <font-awesome-icon
+        icon="comments"
+        style="color:#21202e;padding:0.5rem 0"
+        size="2x"
+      />
       <span class="tooltip">Chat</span>
     </div>
     <div class="icon-holder">
       <font-awesome-icon
         icon="greater-than-equal"
-        style="color:#21202e;"
+        style="color:#21202e;padding:0.5rem 0"
         size="2x"
       />
       <span class="tooltip">Compare prices</span>
     </div>
     <div class="icon-holder">
-      <font-awesome-icon icon="bell" style="color:#21202e;" size="2x" />
+      <font-awesome-icon
+        icon="bell"
+        style="color:#21202e;padding:0.5rem 0"
+        size="2x"
+      />
       <span class="tooltip">Notification</span>
     </div>
     <div class="icon-holder" id="help">
       <font-awesome-icon
         icon="question-circle"
-        style="color:#21202e;"
+        style="color:#21202e;padding:0.5rem 0"
         size="2x"
       />
       <span class="tooltip">Help centre</span>
@@ -51,13 +67,19 @@ export default {
   flex-direction: column;
 }
 .icon-holder {
+  width: 70%;
+  margin: 0 auto;
   position: relative;
   text-align: center;
   margin-bottom: 2em;
   cursor: pointer;
+  border-radius: 10px;
+}
+.icon-holder:hover {
+  background-color: $dark-font;
 }
 #help {
-  margin-top: 20rem;
+  margin-top: 15rem;
 }
 .icon-holder .tooltip {
   visibility: hidden;
