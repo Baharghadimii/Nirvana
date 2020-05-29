6c3bcabe6a1646f4eb59b586797a936e7c3c60b4
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-05-29 13:12:45 -0700

    refactor main.js

--- src/main.js
+++ src/main.js
@@ -3,30 +3,15 @@ import App from './App.vue'
 
 //font-awsome setup
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { faListUl } from '@fortawesome/free-solid-svg-icons'
-import { faFire } from '@fortawesome/free-solid-svg-icons'
-import { faComments } from '@fortawesome/free-solid-svg-icons'
-import { faGreaterThanEqual } from '@fortawesome/free-solid-svg-icons'
-import { faBell } from '@fortawesome/free-solid-svg-icons'
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faListUl, faFire, faComments, faGreaterThanEqual, faBell, faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
 
 //font-awsome setup
-library.add(faSearch);
-library.add(faListUl);
-library.add(faFire);
-library.add(faComments);
-library.add(faGreaterThanEqual);
-library.add(faBell);
-library.add(faQuestionCircle);
-
+library.add(faSearch, faListUl, faFire, faComments, faGreaterThanEqual, faBell, faQuestionCircle);
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
-
-
 
 new Vue({
   render: h => h(App),
