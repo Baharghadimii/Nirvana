3c71f414b1cf4258e47ff30ed6bf59d2032c6e02
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-07-08 16:37:53 -0700

    added donut component

--- src/components/dashboard/index.vue
+++ src/components/dashboard/index.vue
@@ -2,18 +2,21 @@
   <div class="dashboard">
     <Services />
     <Chart />
+    <DonutChart />
   </div>
 </template>
 
 <script>
 import Services from "./services/index.vue";
 import Chart from "./chart/index.vue";
+import DonutChart from "./donut-chart/index";
 
 export default {
   name: "Dashboard",
   components: {
     Services,
-    Chart
+    Chart,
+    DonutChart
   }
 };
 </script>
