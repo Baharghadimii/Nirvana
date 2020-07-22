b2b5bab0e3fd8b2a297a9b6e875223a0b0277fe4
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-07-21 19:18:09 -0700

    added data file for coin item

--- src/data/coins.js
+++ src/data/coins.js
@@ -1,7 +1,32 @@
 export default [
   {
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTd_tuOwUTNb8S8fB7JB1Tah0hZrLhIaZv23Q&usqp=CAU',
-    name: "BitCoin",
+    name: "BTC",
     price: "$12,593"
+  },
+  {
+    img: 'https://bin.bnbstatic.com/static/images/home/coin-logo/BNB.png',
+    name: 'BNB',
+    price: '$17.73'
+  },
+  {
+    img: 'https://bin.bnbstatic.com/static/images/home/coin-logo/ETH.png',
+    name: 'ETH',
+    price: '$244.22'
+  },
+  {
+    img: 'https://bin.bnbstatic.com/static/images/home/coin-logo/XRP.png',
+    name: 'XPR',
+    price: '$0.198600'
+  },
+  {
+    img: 'https://bin.bnbstatic.com/static/images/home/coin-logo/BCH.png',
+    name: 'BCH',
+    price: '$228.87'
+  },
+  {
+    img: 'https://bin.bnbstatic.com/static/images/home/coin-logo/LTC.png',
+    name: 'LTC',
+    price: '$43.80'
   }
 ]
