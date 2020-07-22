5245e31b05f17703088e0a8b9342c5587bb6556e
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-07-21 18:29:56 -0700

    added data file for coin item

--- src/data/coins.js
+++ src/data/coins.js
@@ -1,3 +1,7 @@
-export default {
-
-}
+export default [
+  {
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTd_tuOwUTNb8S8fB7JB1Tah0hZrLhIaZv23Q&usqp=CAU',
+    name: "BitCoin",
+    price: "$12,593"
+  }
+]
